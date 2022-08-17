2d17cd835ae78bbe1c48112645422a3db0ddc819
sam <samantha.underwood@gmail.com>
2022-08-17 15:21:27 -0400

    commit 4 - local host working

--- src/main/java/co/grandcircus/WeatherApiProxy/controller/WeatherController.java
+++ src/main/java/co/grandcircus/WeatherApiProxy/controller/WeatherController.java
@@ -8,11 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import co.grandcircus.WeatherApiProxy.models.forecastModels.Periods;
 import co.grandcircus.WeatherApiProxy.service.WeatherApiService;
 
-@Controller
+@RestController
 public class WeatherController {
 
 	@Autowired
